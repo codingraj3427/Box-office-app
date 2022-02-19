b459a76726e5ba6b26b0de099c56bd54030b2470
coderaj2001 <biswajit05101963@gmail.com>
2022-02-19 23:32:20 +0530

    We have created a switch statement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch,Route} from 'react-router-dom'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Switch>
+    <Route exact path="/">
+     MY name is Rajarshi Chakraborty.
+   </Route>
+
+   <Route exact path="/starred">
+      This is starred
+   </Route>
+
+   <Route>
+     This is 404 page
+   </Route>
+  </Switch>;
 }
 
 export default App;
