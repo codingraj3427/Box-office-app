0b4c62b246f6263a254c783aa8baf0bec9ef7bb7
coderaj2001 <biswajit05101963@gmail.com>
2022-02-24 17:06:52 +0530

    Added actor functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,14 +10,15 @@ function Home() {
 
   const[input,setInput]=useState('');
   const[result,setResult]=useState(null);
+  const[searchOption,setsearchOption]=useState('');
 
-
+  const isShowSearch=searchOption==='shows';
 
   const onSearch=()=>
   {
     
 
-    apiGet(`/search/shows?q=${input}`).then(result=>
+    apiGet(`/search/${searchOption}?q=${input}`).then(result=>
       { 
         setResult(result);
       });
@@ -41,17 +42,30 @@ function Home() {
     }
   }
 
+
+  const onRadioChange=(ev)=>
+  {
+      setsearchOption(ev.target.value);
+      
+  }
+
+  console.log(searchOption);
+
+
   const renderResult= ()=>
   {
-    if(result && result.length==0)
+    if(result && result.length===0)
     {   
       return <div>No Result</div>
     }
 
     if(result && result.length>0){
     
-      return <div>{result.map( (item)=>(<div key={item.show.id}>{item.show.name}</div>))}</div>
-      
+      return(
+
+          result[0].show ? result.map( (item)=>(<div key={item.show.id}>{item.show.name}</div>)) : result.map( (item)=>(<div key={item.person.id}>{item.person.name}</div>))
+        
+        );
     }
 
     return null;
@@ -61,7 +75,32 @@ function Home() {
   
   return (
     <MainPageLayout>
-      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+      <input type="text" placeholder="Search something"onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+
+
+      <div>
+        <label htmlFor='Shows-search'>
+          Shows
+          <input id="Shows-search
+          "type="radio" 
+          value="shows"
+          checked={isShowSearch} 
+          onChange={onRadioChange}/>
+        </label>
+
+        <label htmlFor='Actor-search'>
+          Actors
+          <input id="Actor-search"
+          type="radio" 
+          value="people" 
+          checked={!isShowSearch}
+          onChange={onRadioChange}/>
+        </label>
+      </div>
+
+
+
+
       <button 
       type="button" onClick={onSearch}>Search
       </button>
