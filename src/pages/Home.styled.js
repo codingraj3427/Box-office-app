03d04fd21b8273647961670898cba40053aafa0a
coderaj2001 <biswajit05101963@gmail.com>
2022-02-28 01:13:09 +0530

    We have completed the app with animation

--- src/pages/Home.styled.js
+++ src/pages/Home.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const SearchInput = styled.input`
   display: block;
   font-family: 'Roboto', sans-serif;
-  width: 200px;
+  width: 400px;
   margin: auto;
   outline: none;
   padding: 13px 15px;
