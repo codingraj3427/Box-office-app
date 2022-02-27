c1571088810fb501389c1d30829fd7468981e2af
coderaj2001 <biswajit05101963@gmail.com>
2022-02-28 00:23:31 +0530

    Successfully created hooks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,124 +1,92 @@
-import React,{useState,useEffect} from 'react'
-import ActorGrid from '../components/actor/ActorGrid';
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 import ShowGrid from '../components/show/ShowGrid';
-import {apiGet} from '../misc/config';
+import ActorGrid from '../components/actor/ActorGrid';
+import { useLastQuery } from '../misc/custom-hooks';
 
+const Home = () => {
+  const [input, setInput] = useLastQuery();
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
 
-
-
-
-
-function Home() {
-
-  const[input,setInput]=useState('');
-  const[result,setResult]=useState(null);
-  const[searchOption,setsearchOption]=useState('');
-
-  const isShowSearch=searchOption==='shows';
-
-  useEffect(()=>{
-    console.log("USe effect run  ");
-
-    return ()=>
-    {
-      console.log("exit")
-    };
-  },[searchOption])     //It requires two components ad 
-
-  const onSearch=()=>
-  {
-    
-
-    apiGet(`/search/${searchOption}?q=${input}`).then(result=>
-      { 
-        setResult(result);
-      });
-
-     
+  const isShowsSearch = searchOption === 'shows';
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
-
-  const onInputChange=(ev)=>
-  { 
+  const onInputChange = ev => {
     setInput(ev.target.value);
-    console.log(ev.target.value);
   };
 
-
-  const onKeyDown=(ev)=>
-  {
-    if(ev.keyCode===13)
-    {
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
       onSearch();
     }
-  }
+  };
 
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
 
-  const onRadioChange=(ev)=>
-  {
-      setsearchOption(ev.target.value);
-      
-  }
-
-  console.log(searchOption);
-
-
-  const renderResult= ()=>
-  {
-    if(result && result.length===0)
-    {   
-      return <div>No Result</div>
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
     }
 
-    if(result && result.length>0){
-    
-      return(
-
-          result[0].show ? <ShowGrid data={result}/> : <ActorGrid  data={result}/>
-        
-        );
+    if (results && results.length > 0) {
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
 
     return null;
-  }
+  };
 
-
-  
   return (
     <MainPageLayout>
-      <input type="text" placeholder="Search something"onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
-
+      <input
+        type="text"
+        placeholder="Search for something"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
 
       <div>
-        <label htmlFor='Shows-search'>
+        <label htmlFor="shows-search">
           Shows
-          <input id="Shows-search
-          "type="radio" 
-          value="shows"
-          checked={isShowSearch} 
-          onChange={onRadioChange}/>
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
         </label>
 
-        <label htmlFor='Actor-search'>
+        <label htmlFor="actors-search">
           Actors
-          <input id="Actor-search"
-          type="radio" 
-          value="people" 
-          checked={!isShowSearch}
-          onChange={onRadioChange}/>
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
         </label>
       </div>
 
-
-
-
-      <button 
-      type="button" onClick={onSearch}>Search
+      <button type="button" onClick={onSearch}>
+        Search
       </button>
-      {renderResult()}
+      {renderResults()}
     </MainPageLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
